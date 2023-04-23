f2b3c6ae3cdfc5ba0ae7319450953a45981ea6fd
joseph <joseph.ketterer@gmail.com>
2023-04-23 09:24:57 +0100

    css

--- wp-content/themes/wsw/index.php
+++ wp-content/themes/wsw/index.php
@@ -2,21 +2,21 @@
 <main class="main-body">
   <div class="lead singlecontainer news">
           <?php if (have_posts()) {
-          	while (have_posts()) {
-          		the_post(); ?>
+            while (have_posts()) {
+              the_post(); ?>
               <article class="post-item">
                 <h2 class="headline"><a href="<?php esc_url(
-                	the_permalink()
+                  the_permalink()
                 ); ?>"><?php echo esc_html__(the_title()); ?></a></h2>
                 
                 <div>
                   <p class="highlight-link">Posted by <span class="blog-category"><?php esc_url(
-                  	the_author_posts_link()
+                    the_author_posts_link()
                   ); ?></span> on <?php esc_html__(
-	the_time('n.j.y')
+  the_time('n.j.y')
 ); ?> in <span class="blog-category"><?php echo get_the_category_list(
- 	', '
- ); ?></span></p>
+  ', '
+); ?></span></p>
                 </div>
           
                 <div>
@@ -24,19 +24,19 @@
                   <a href="<?php esc_url(the_permalink()); ?>" class="readmore"
               >Read more
               <div class="container-chevron" id="<?php echo esc_html__(
-              	get_the_ID(the_post())
+                get_the_ID(the_post())
               ); ?>">
                 <i class="fa-solid fa-chevron-right"></i
                 ><i class="fa-solid fa-chevron-right"></i></div
             ></a>
                 </div>
-         
+        
             </article>
             <?php
-          	}
+            }
           } ?>
         
-            <div><p>  <?php echo paginate_links(); ?> <p></div> 
+          <div><p>  <?php echo paginate_links(); ?> <p></div> 
             
           
          
